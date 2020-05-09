2e17cb644ea2a9bb00dfb28cff43ea577c1a0445
Asish Samantaray <samantaray.asish04@gmail.com>
2020-05-10 03:22:32 +0530

    Retrieve String data from firebase.

--- app/src/main/java/com/asish/firebaseintro/MainActivity.java
+++ app/src/main/java/com/asish/firebaseintro/MainActivity.java
@@ -1,11 +1,16 @@
 package com.asish.firebaseintro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +28,19 @@ public class MainActivity extends AppCompatActivity {
         // Store Data in
         databaseReference.setValue("Hello World!!");
 
+        // Retrieve data from firebase..
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String data = dataSnapshot.getValue(String.class);
+                Toast.makeText(MainActivity.this, data, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
