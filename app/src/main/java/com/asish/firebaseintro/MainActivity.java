29f1c02f90b8e0af8404273cb66019f2cdc219b3
Asish Samantaray <samantaray.asish04@gmail.com>
2020-05-10 03:17:42 +0530

    Store String Data in firebase.

--- app/src/main/java/com/asish/firebaseintro/MainActivity.java
+++ app/src/main/java/com/asish/firebaseintro/MainActivity.java
@@ -9,11 +9,19 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Message");
+
+        // Store Data in
+        databaseReference.setValue("Hello World!!");
 
 
     }
